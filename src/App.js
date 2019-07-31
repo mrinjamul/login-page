3d9c92f2bc40314e50ba007fdf498f866e48ef6d
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-07-31 20:48:58 +0530

    Add login button with function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/*
+Challenge:
+
+Given a stateless functional component:
+1. Follow the steps necessary to add state to it,
+    // class-based component
+    // constructor method
+2. Have state keep track of whether the user is logged in or not
+    // isLoggedIn: Boolean (true or false)
+3. Add a button that logs the user in/out
+    // event listener (onClick)
+    a. extra challenge - make the button display "log in" if they're not logged in and "log out" if they are
+        // Conditional Rendering
+4. Display text that says "Logged in" if the user is logged in, or "Logged out" if they're not.
+    // Conditional Rendering
+*/
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoggedIn: false
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick() {
+        this.setState(prevState => {
+            return {
+                isLoggedIn: !prevState.isLoggedIn
+            }
+        })
+    }
+
+    render() {
+        let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+        return (
+            <div>
+                <button onClick={this.handleClick}>{buttonText}</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
